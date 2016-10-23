ed89f171c882c4164706d8a818d4968ad9d32104
mnikn <82193124>
2016-10-23 14:20:23 +0800

    add retrofit in library

---
+++ library/src/main/java/com/mnikn/library/support/retrofit/RetrofitBuilder.java
@@ -0,0 +1,31 @@
+package com.mnikn.library.support.retrofit;
+
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * @author <a href="mailto:iamtruelyking@gmail.com">mnikn</a>
+ */
+public class RetrofitBuilder {
+
+    private static RetrofitBuilder INSTANCE;
+
+    private RetrofitBuilder() {}
+
+    public static RetrofitBuilder getInstance(){
+        if(INSTANCE == null){
+            INSTANCE = new RetrofitBuilder();
+        }
+        return INSTANCE;
+    }
+
+    public Retrofit retrofit(String baseUrl){
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+    }
+}
